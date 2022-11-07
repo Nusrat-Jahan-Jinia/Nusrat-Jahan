7143fa67fc4bd12e724e460e3a40c6f3842f4d8a
nusrat jahan <nusrat.jahan@dsinnovators.com>
2022-11-07 12:51:33 +0600

    Changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,15 +1,15 @@
 const siteMetadata = {
-  title: 'Starter Blog',
+  title: 'Mst Nusrat Jahan',
   author: 'Mst Nusrat Jahan',
   headerTitle: 'Nusrat',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Mst Nusrat Jahan',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.jpg',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/avatar.jpg',
   email: 'nusratcode14@gmail.com',
   github: 'https://github.com/Nusrat-Jahan-Jinia',
   // twitter: 'https://twitter.com/Twitter',
